68f6c4c8d499013356edfd5e3a77b8e059f996b4
chuluq <choirulchuluq10@gmail.com>
2022-05-31 13:54:00 +0700

    add about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,5 +1,12 @@
+import "./styles.css";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="container">
+      <h1>About</h1>
+      <p>Search Movies using OMDB API</p>
+    </div>
+  );
 };
 
 export default About;
